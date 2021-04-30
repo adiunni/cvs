2c2ea4ebc66d5f77bae7cc11876576061049d858
CharieBlastX7 <61608668+CharieBlastX7@users.noreply.github.com>
2021-04-30 22:55:23 +0530

    Added slider for image slideshow

---
+++ components/slider/SliderComp.js
@@ -0,0 +1,52 @@
+
+import Slider from 'react-slick'
+import styles from './SliderComp.module.css'
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
+import { Center,Box } from '@chakra-ui/layout';
+
+export default function SliderComp(){
+
+    const settings = {
+        dots:false,
+        infinite:true,
+        speed:700,
+        slidesToShow:1,
+        autoplay:true,
+        autoplaySpeed:3000,
+        loop:true,
+        centerMode:true,
+    };
+
+    return (
+        <Box height={['170vh,130vh,70vh']} pl={2} pr={2}>
+        <Slider {...settings}>
+            <div className={[styles.main,styles.background1].join(" ")}>
+                <Center>
+                    <h1 className={styles.h1}>1920x1080 1</h1>
+                </Center>
+            </div>
+            <div className={[styles.main,styles.background2].join(" ")}>
+                <Center>
+                    <h1 className={styles.h1}>1920x1080 2</h1>
+                </Center>
+            </div>
+            <div className={[styles.main,styles.background3].join(" ")}>
+                <Center>
+                    <h1 className={styles.h1}>1920x1080 3</h1>
+                </Center>
+            </div>
+            <div className={[styles.main,styles.background4].join(" ")}>
+                <Center>
+                    <h1 className={styles.h1}>1920x1080 4</h1>
+                </Center>
+            </div>
+            <div className={[styles.main,styles.background5].join(" ")}>
+                <Center>
+                    <h1 className={styles.h1}>1920x1080 5</h1>
+                </Center>
+            </div>
+        </Slider>
+        </Box>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Navbar from '../components/navbar/Navbar'
+import SliderComp from '../components/slider/SliderComp'
 import React from 'react' 
+import styles from '../styles/Home.module.css'
 
 
 export default function Home(){ 
 
   return (
-    <>
+    <div className={styles.root}>
       <Navbar/>
-    </>
+      <SliderComp/>
+    </div>
   )
 }  
